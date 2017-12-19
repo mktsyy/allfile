42d30d66ca0476f8f1157ba29e4018b9281d3402
mktsyy <mktsyy@gmail.com>
2017-12-19 14:58:56 +0800

    testbackground
    Signed-off-by: mktsyy <mktsyy@gmail.com>

---
+++ testbackground/readscript.js
@@ -0,0 +1,6 @@
+
+//-------------------- badge演示 ------------------------//
+
+chrome.browserAction.setBadgeText({text: '租'});
+chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255]});
+			
