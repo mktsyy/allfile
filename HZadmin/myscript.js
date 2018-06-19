1e20ed748a1b9b6a45f9dfeca214baa012b5f8c0
mktsyy <mktsyy@gmail.com>
2018-06-19 16:19:36 +0800

    嗨住后台

---
+++ HZadmin/myscript.js
@@ -0,0 +1,40 @@
+//根据区域自动选择电话号码
+if (document.getElementById("ddl_region").value = 24) {  //徐汇
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 43) { //长宁
+    document.getElementById("accountDiv").childNodes[9].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 53) { //浦东
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) { //
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+} else if (document.getElementById("ddl_region").value = 24) {
+    document.getElementById("accountDiv").childNodes[3].childNodes[0].checked = true;
+}
+
+
